665e2b23624adaf25a4c36daca64a6cb7d2b4b64
decoss-coder <142638887+decoss-coder@users.noreply.github.com>
2026-05-25 23:49:46 +0000

    Split scolarite and inscription point menus

--- resources/views/partials/sidebar-nav-render.blade.php
+++ resources/views/partials/sidebar-nav-render.blade.php
@@ -36,10 +36,19 @@
 
     if ($canSeePointPostes) {
         $extraItems[] = [
-            'r' => 'finances.point-postes.index',
-            'match' => 'finances.point-postes.*',
-            'label' => 'Point scolarité & inscription',
-            'd' => 'M9 17v-2a4 4 0 014-4h6m-6 0a4 4 0 00-4 4v2m4-6V7a4 4 0 118 0v4m-8 0h8M5 21h14a2 2 0 002-2v-2a2 2 0 00-2-2H5a2 2 0 00-2 2v2a2 2 0 002 2z',
+            'r' => 'finances.index',
+            'rp' => ['point' => 'inscription'],
+            'match' => 'finances.index',
+            'label' => 'Point inscription',
+            'd' => 'M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z',
+        ];
+
+        $extraItems[] = [
+            'r' => 'finances.index',
+            'rp' => ['point' => 'scolarite'],
+            'match' => 'finances.index',
+            'label' => 'Point scolarité',
+            'd' => 'M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2m2 4h10a2 2 0 002-2v-6a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2zm7-5a2 2 0 11-4 0 2 2 0 014 0z',
         ];
     }
 
