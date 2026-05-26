7d677cc11ca92a1f3c70fc6a36d5b1857a4b052c
decoss-coder <142638887+decoss-coder@users.noreply.github.com>
2026-05-26 19:45:06 +0000

    Add rooms menu entry

--- resources/views/partials/sidebar-nav-render.blade.php
+++ resources/views/partials/sidebar-nav-render.blade.php
@@ -41,6 +41,13 @@
             'label' => 'Disciplines',
             'd' => 'M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.746 0 3.332.477 4.5 1.253v13C19.832 18.477 18.246 18 16.5 18c-1.746 0-3.332.477-4.5 1.253',
         ];
+
+        $schoolSettingItems[] = [
+            'r' => 'emploi-du-temps.salles.index',
+            'match' => 'emploi-du-temps.salles.*',
+            'label' => 'Salles',
+            'd' => 'M8 3h8l4 4v14a2 2 0 01-2 2H6a2 2 0 01-2-2V7l4-4z M9 21v-6h6v6 M9 10h.01 M12 10h.01 M15 10h.01',
+        ];
     }
 
     if (! empty($schoolSettingItems)) {
